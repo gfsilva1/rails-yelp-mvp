23bcbe964d66b28cc6e90290dd2ad6bef0df20b1
Guilherme Silva <gfsilva@hotmail.com>
2022-04-18 09:09:06 -0300

    rev

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :restaurants, only: [:create, :index, :show, :new] do
-    resources :reviews, only: [:create]
+    resources :reviews, only: [ :new, :create ]
   end
 end
